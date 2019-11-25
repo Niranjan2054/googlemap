b8f5ec18ddc51eb5c60bb4cbd503126ef8db49cf
Niranjan2054 <ckim6189@gmail.com>
2019-11-25 19:36:02 +0545

    Google Map Integration

---
+++ app/src/main/java/com/niranjan2054/googlemap/MapsActivity.java
@@ -0,0 +1,51 @@
+package com.niranjan2054.googlemap;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.VisibleRegion;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng bhaktapur = new LatLng(27.6711702,85.4322538);
+        mMap.addMarker(new MarkerOptions().position(bhaktapur).title("Bhaktapur")).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bhaktapur));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(bhaktapur));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(bhaktapur,16));
+    }
+}
